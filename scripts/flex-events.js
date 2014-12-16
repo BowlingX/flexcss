bb2197c622f2dc02f6356e7a7e8419fb852209ac
David Heidrich <me@bowlingx.com>
2014-12-16 23:33:46 +0100

    fixed IE10 error with custom events

--- scripts/flex-events.js
+++ scripts/flex-events.js
@@ -9,18 +9,16 @@ void function (document, window, $) {
      * Polyfill CustomEvent for Browser who are not supported
      */
     void function () {
-        if (!('CustomEvent' in window)) {
-            var CustomEvent = function CustomEvent(event, params) {
-                params = params || {bubbles: false, cancelable: false, detail: undefined};
-                var evt = document.createEvent('CustomEvent');
-                evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-                return evt;
-            };
+        var CustomEvent = function CustomEvent(event, params) {
+            params = params || {bubbles: false, cancelable: false, detail: undefined};
+            var evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        };
 
-            CustomEvent.prototype = window.Event.prototype;
+        CustomEvent.prototype = window.Event.prototype;
 
-            window.CustomEvent = CustomEvent;
-        }
+        window.CustomEvent = CustomEvent;
     }();
 
     if (!window.FlexCss) {
