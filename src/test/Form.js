9e8ac0338b9a41d798c298f958b148f3cf5dfa74
David Heidrich <me@bowlingx.com>
2015-02-08 16:00:30 -0500

    add basic tests for tooltips

--- src/test/Form.js
+++ src/test/Form.js
@@ -1,12 +1,12 @@
 import Form from 'Form';
 
 describe("Construct a Form Element", function() {
-    it("A valid form object without parameters needs to be constructed", function() {
+    it("A valid form object without options needs to be constructed", function() {
         var form = new Form(document.createElement('form'));
         expect(form).toBe(form);
     });
 
-    it("A valid form object with overwritten default parameters needs to be constructed", function(){
+    it("A valid form object with overwritten default options needs to be constructed", function(){
         var form = new Form(document.createElement('form'), {createTooltips:false, appendError:true});
         expect(form).toBe(form);
 
---
+++ src/test/Tooltip.js
@@ -0,0 +1,8 @@
+import Tooltip from 'Tooltip';
+
+describe("Construct a Tooltip Element", function() {
+    it("A valid tooltip object without options needs to be constructed", function() {
+        var tooltip = new Tooltip(document.createElement('div'));
+        expect(tooltip).toBe(tooltip);
+    });
+});
