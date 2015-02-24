d0a3f36560409597207061947f7f5a7e7484507a
David Heidrich <me@bowlingx.com>
2015-02-24 16:52:09 +0100

    setup next source

--- scripts/flex-lightbox.js
+++ scripts/flex-lightbox.js
@@ -249,6 +249,7 @@ void function (document, window, $) {
                             target = next;
                             nextImgObject.src = nextSource;
                             imageContainer.classList.add('loading');
+                            imageContainer.style['backgroundImage'] = 'url('+nextSource+')';
                             nextImgObject.addEventListener('load', function () {
                                 img.src = nextSource;
                                 self._setupMaxWidthHeight(nextThumb, img, nextImgObject);
