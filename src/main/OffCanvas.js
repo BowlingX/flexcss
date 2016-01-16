00fb0c5db6bc7e99a0b46e05afd39d6f6c3fbb47
David Heidrich <me@bowlingx.com>
2016-01-16 01:27:00 +0100

    added toggle event to offcanvas

--- src/main/OffCanvas.js
+++ src/main/OffCanvas.js
@@ -114,57 +114,57 @@ class OffCanvas {
         if (!disableTouch) {
             navigationContainer.addEventListener('touchstart', (e) => {
                 requestAnimationFrame(() => {
-                if (shouldNotTouch()) {
-                    return;
-                }
-                touched = e.touches[0].clientX;
-                const target = navigationContainer;
-                const style = target.style;
-                target.mustHide = false;
-                style.transition = 'transform 0s ease';
-                style.webkitTransition = '-webkit-transform 0s ease';
+                    if (shouldNotTouch()) {
+                        return;
+                    }
+                    touched = e.touches[0].clientX;
+                    const target = navigationContainer;
+                    const style = target.style;
+                    target.mustHide = false;
+                    style.transition = 'transform 0s ease';
+                    style.webkitTransition = '-webkit-transform 0s ease';
+                });
             });
-        });
             navigationContainer.addEventListener('touchmove', (e) => {
                 if (shouldNotTouch()) {
-                return;
-            }
-            const x = e.touches[0].clientX;
-            const target = navigationContainer;
-            const style = target.style;
-            const calc = touched - x;
-            const bounds = target.getBoundingClientRect();
-            const compare = factor > 0 ? calc <= 0 : calc >= 0;
-            if (compare) {
-                target.mustHide = factor > 0 ? calc * -1 >
-                bounds.width / HIDE_FACTOR : calc > bounds.width / HIDE_FACTOR;
-                style.transform = 'translate3d(' + (calc * -1) + 'px,0,0)';
-                style.webkitTransform = 'translate3d(' + (calc * -1) + 'px,0,0)';
-            }
-        });
-            navigationContainer.addEventListener('touchend', () => {
-                requestAnimationFrame(() => {
-                if (shouldNotTouch()) {
                     return;
                 }
+                const x = e.touches[0].clientX;
                 const target = navigationContainer;
                 const style = target.style;
-                if (target.mustHide) {
-                    const width = target.getBoundingClientRect().width * factor;
-                    style.transition = 'transform .2s ease';
-                    style.webkitTransition = '-webkit-transform .2s ease';
-
-                    style.transform = 'translate3d(' + width + 'px,0,0)';
-                    style.webkitTransform = 'translate3d(' + width + 'px,0,0)';
-                    this._remove(() => {
-                        resetStyles(style);
-                });
-                    this._removeInstant();
-                } else {
-                    resetStyles(style);
+                const calc = touched - x;
+                const bounds = target.getBoundingClientRect();
+                const compare = factor > 0 ? calc <= 0 : calc >= 0;
+                if (compare) {
+                    target.mustHide = factor > 0 ? calc * -1 >
+                    bounds.width / HIDE_FACTOR : calc > bounds.width / HIDE_FACTOR;
+                    style.transform = 'translate3d(' + (calc * -1) + 'px,0,0)';
+                    style.webkitTransform = 'translate3d(' + (calc * -1) + 'px,0,0)';
                 }
             });
-        });
+            navigationContainer.addEventListener('touchend', () => {
+                requestAnimationFrame(() => {
+                    if (shouldNotTouch()) {
+                        return;
+                    }
+                    const target = navigationContainer;
+                    const style = target.style;
+                    if (target.mustHide) {
+                        const width = target.getBoundingClientRect().width * factor;
+                        style.transition = 'transform .2s ease';
+                        style.webkitTransition = '-webkit-transform .2s ease';
+
+                        style.transform = 'translate3d(' + width + 'px,0,0)';
+                        style.webkitTransform = 'translate3d(' + width + 'px,0,0)';
+                        this._remove(() => {
+                            resetStyles(style);
+                        });
+                        this._removeInstant();
+                    } else {
+                        resetStyles(style);
+                    }
+                });
+            });
         }
     }
 
@@ -175,18 +175,18 @@ class OffCanvas {
         Util.addEventOnce(Settings.getTransitionEvent(), this.navigationContainer, () => {
             // add timeout because transition event fires a little to early
             setTimeout(() => {
-            requestAnimationFrame(() => {
-                const body = global.document.body;
-            OffCanvas.currentOpen = null;
-            body.classList.remove(TOGGLE_CLASS);
-            body.classList.remove(this.darkenerClassToggle);
-            if (callback) {
-                Event.dispatchAndFire(this.navigationContainer, EVENT_TOGGLE)
-                callback();
-            }
+                requestAnimationFrame(() => {
+                    const body = global.document.body;
+                    OffCanvas.currentOpen = null;
+                    body.classList.remove(TOGGLE_CLASS);
+                    body.classList.remove(this.darkenerClassToggle);
+                    Event.dispatchAndFire(this.navigationContainer, EVENT_TOGGLE);
+                    if (callback) {
+                        callback();
+                    }
+                });
+            }, Settings.get().darkenerFadeDelay);
         });
-        }, Settings.get().darkenerFadeDelay);
-    });
     }
 
     /**
@@ -214,17 +214,17 @@ class OffCanvas {
             this._remove();
             this._removeInstant(navigationControllerClassList);
         } else if (!OffCanvas.currentOpen) {
+
+            Util.addEventOnce(Settings.getTransitionEvent(), this.navigationContainer, () => {
+                Event.dispatchAndFire(this.navigationContainer, EVENT_TOGGLE)
+            });
+
             OffCanvas.currentOpen = this;
             bodyClass.add(DARKENER_CLASS_INSTANT_TOGGLE);
             bodyClass.add(TOGGLE_CLASS);
             bodyClass.add(DARKENER_CLASS_TOGGLE);
             darkenerClass.add(INIT_CLASS);
             navigationControllerClassList.add(OPEN_CLASS);
-
-            Util.addEventOnce(Settings.getTransitionEvent(), this.navigationContainer, () => {
-                Event.dispatchAndFire(this.navigationContainer, EVENT_TOGGLE)
-        });
-
         }
     }
 
@@ -236,20 +236,20 @@ class OffCanvas {
         const thisDelegate = delegate || global.document;
         thisDelegate.addEventListener(Settings.getTabEvent(), (e) => {
             if (OffCanvas.currentOpen && OffCanvas.currentOpen !== this) {
-            return;
-        }
-        const id = this.navigationContainerId;
-        const validTarget = e.target.getAttribute(ATTR_TARGET) === id;
-        if (!Util.isPartOfNode(e.target, this.navigationContainer)) {
-            if (validTarget || (OffCanvas.currentOpen === this && e.target === this.darkener)) {
-                this._toggle(e);
+                return;
             }
-        } else {
-            if (e.target.hasAttribute(ATTR_CLOSE_SIDEBAR)) {
-                this._toggle(e);
+            const id = this.navigationContainerId;
+            const validTarget = e.target.getAttribute(ATTR_TARGET) === id;
+            if (!Util.isPartOfNode(e.target, this.navigationContainer)) {
+                if (validTarget || (OffCanvas.currentOpen === this && e.target === this.darkener)) {
+                    this._toggle(e);
+                }
+            } else {
+                if (e.target.hasAttribute(ATTR_CLOSE_SIDEBAR)) {
+                    this._toggle(e);
+                }
             }
-        }
-    });
+        });
     }
 }
 
