cd7ff95e2b89aa6880a779c17e6fad54019beea8
David Heidrich <me@bowlingx.com>
2016-02-04 14:35:12 +0100

    feat(canvas): any open layer will now hide body/html overflow

--- src/test/Tooltip.js
+++ src/test/Tooltip.js
@@ -2,18 +2,17 @@
 
 import Tooltip from 'Tooltip';
 
-describe("Construct a Tooltip Element", function () {
-
+describe("Construct a Tooltip Element", () => {
     it("A valid tooltip object without options needs to be constructed", () => {
-        let tooltip = new Tooltip(document.createElement('div'));
+        const tooltip = new Tooltip(document.createElement('div'));
         expect(tooltip instanceof Tooltip).toBe(true);
     });
 
     it("A Tooltip should bind events and destroy itself after", () => {
-        let tooltip = new Tooltip(document.createElement('div'));
+        const tooltip = new Tooltip(document.createElement('div'));
         tooltip.registerEvents();
         expect(tooltip.listeners.length).toBe(2);
         tooltip.destroy();
         expect(tooltip.listeners.length).toBe(0);
-    })
+    });
 });
